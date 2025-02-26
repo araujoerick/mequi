350c1b030e3c5edd5ff4656bd24e150e80909915
Erick Araújo <araujoerick.dev@gmail.com>
2025-02-25 22:16:24 -0300

    ✨ feat: add RestaurantMenuPage and Header component

---
+++ src/app/[slug]/menu/page.tsx
@@ -0,0 +1,38 @@
+import { notFound } from "next/navigation";
+
+import { getRestaurantBySlug } from "@/data/get-restaurant-by-slug";
+
+import RestaurantHeader from "./components/header";
+
+interface RestaurantMenuPageProps {
+  params: Promise<{ slug: string }>;
+  searchParams: Promise<{ consumptionMethod: string }>;
+}
+
+const isConsumptionMethod = (consumptionMethod: string) => {
+  return ["DINE_IN", "TAKEAWAY"].includes(consumptionMethod.toUpperCase());
+};
+
+const RestaurantMenuPage = async ({
+  params,
+  searchParams,
+}: RestaurantMenuPageProps) => {
+  const { slug } = await params;
+  const restaurant = await getRestaurantBySlug(slug);
+  if (!restaurant) {
+    return notFound();
+  }
+
+  const { consumptionMethod } = await searchParams;
+  if (!isConsumptionMethod(consumptionMethod)) {
+    return notFound();
+  }
+
+  return (
+    <main>
+      <RestaurantHeader restaurant={restaurant} />
+    </main>
+  );
+};
+
+export default RestaurantMenuPage;
