f5169bfec10b4981352fe38205c75dc95783c053
Erick Araújo <araujoerick.dev@gmail.com>
2025-02-26 19:04:14 -0300

    ✨ feat: add ProductHeader component

---
+++ src/app/[slug]/menu/[productId]/page.tsx
@@ -0,0 +1,35 @@
+import { notFound } from "next/navigation";
+
+import { db } from "@/lib/prisma";
+
+import ProductHeader from "./components/product-header";
+
+interface ProductPageProps {
+  params: Promise<{ slug: string; productId: string }>;
+}
+
+const ProductPage = async ({ params }: ProductPageProps) => {
+  const { slug, productId } = await params;
+  const product = await db.product.findUnique({ where: { id: productId } });
+  if (!product) {
+    return notFound();
+  }
+
+  return (
+    <section>
+      <ProductHeader product={product} />
+      <div className="-mt-5 rounded-lg bg-white p-5">
+        <h2>{product.name}</h2>
+        <p>{product.description}</p>
+        <p>
+          {new Intl.NumberFormat("pt-BR", {
+            style: "currency",
+            currency: "BRL",
+          }).format(product.price)}
+        </p>
+      </div>
+    </section>
+  );
+};
+
+export default ProductPage;
