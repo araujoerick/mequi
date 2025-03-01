13ea1abd74db51056cd1d3e1c4db509190622f90
Erick Araújo <araujoerick.dev@gmail.com>
2025-02-28 22:01:29 -0300

    ✨ feat: ensure cart total is shown

--- src/app/[slug]/menu/components/cart-sheet.tsx
+++ src/app/[slug]/menu/components/cart-sheet.tsx
@@ -1,17 +1,21 @@
 import { useContext } from "react";
 
+import { Button } from "@/components/ui/button";
+import { Card, CardContent } from "@/components/ui/card";
+import { ScrollArea } from "@/components/ui/scroll-area";
 import {
   Sheet,
   SheetContent,
   SheetHeader,
   SheetTitle,
 } from "@/components/ui/sheet";
+import { formatCurrency } from "@/helpers/format-currency";
 
 import { CartContext } from "../contexts/cart";
 import CartItem from "./cart-product-item";
 
 const CartSheet = () => {
-  const { isOpen, toggleCart, products } = useContext(CartContext);
+  const { isOpen, toggleCart, products, total } = useContext(CartContext);
   return (
     <Sheet open={isOpen} onOpenChange={toggleCart}>
       <SheetContent
@@ -21,9 +25,34 @@ const CartSheet = () => {
         <SheetHeader>
           <SheetTitle className="mb-3.5 text-left">Sacola</SheetTitle>
         </SheetHeader>
-        {products.map((product) => (
-          <CartItem key={product.id} product={product} />
-        ))}
+        <div className="flex h-[calc(100%-2.5rem)] flex-col justify-between gap-5">
+          <ScrollArea>
+            <div>
+              {products.map((product) => (
+                <CartItem key={product.id} product={product} />
+              ))}
+              {products.map((product) => (
+                <CartItem key={product.id} product={product} />
+              ))}
+              {products.map((product) => (
+                <CartItem key={product.id} product={product} />
+              ))}
+            </div>
+          </ScrollArea>
+          <div className="space-y-6">
+            <Card>
+              <CardContent className="p-5">
+                <div className="flex justify-between">
+                  <p className="text-sm text-muted-foreground">Total</p>
+                  <p className="text-sm font-semibold">
+                    {formatCurrency(total)}
+                  </p>
+                </div>
+              </CardContent>
+            </Card>
+            <Button className="w-full rounded-full">Finalizar pedido</Button>
+          </div>
+        </div>
       </SheetContent>
     </Sheet>
   );
