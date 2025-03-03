d9567aeed999f4532e2976572e4ee07427f56f36
Erick Araújo <araujoerick.dev@gmail.com>
2025-03-03 00:04:52 -0300

    ✨ feat: ensure back button work as expected

--- src/app/[slug]/menu/components/header-buttons.tsx
+++ src/app/[slug]/menu/components/header-buttons.tsx
@@ -1,5 +1,5 @@
 import { ChevronLeftIcon, ScrollTextIcon } from "lucide-react";
-import { usePathname, useRouter } from "next/navigation";
+import { useParams, usePathname, useRouter } from "next/navigation";
 import React from "react";
 
 import { Button } from "@/components/ui/button";
@@ -7,8 +7,12 @@ import { Button } from "@/components/ui/button";
 const HeaderButtons = () => {
   const router = useRouter();
   const handleBackClick = () => router.back();
+
   const pathname = usePathname();
   const isOrderPage = pathname.includes("/orders");
+
+  const { slug } = useParams<{ slug: string }>();
+  const handleOrdersClick = () => router.push(`/${slug}/orders`);
 
   return (
     <>
@@ -23,6 +27,7 @@ const HeaderButtons = () => {
       <Button
         variant="secondary"
         size="icon"
+        onClick={handleOrdersClick}
         className={`absolute right-4 top-4 z-50 rounded-full ${isOrderPage ? "pointer-events-none" : "bg-white"}`}
       >
         <ScrollTextIcon />
